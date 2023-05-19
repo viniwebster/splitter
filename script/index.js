83c5d097450cc5e6c2f469c34d56194b5fd8ef1c
vinicius webster <viniciuswebster99@gmail.com>
2023-05-19 18:54:03 -0300

    js feat

--- script/index.js
+++ script/index.js
@@ -4,27 +4,39 @@ const tipAmount = document.getElementById('tip_value');
 const total = document.getElementById('total');
 const reset = document.getElementById('reset');
 const tipCustom = document.getElementById('tip');
+const error = document.getElementById('error');
 
+//objeto que armazena os valores para manipula-los
 const values = {
-    bill : 0,
+    bill: 0,
     people: 0,
     tipValue: 0,
     customTip: "",
-    tipResult: function () {
+    tipResult: function () {    /* função calcula o total da conta e da gorjeta */
         let result = (this.bill / this.people);
         let tipResult = ((this.tipValue / 100) * result);
         return { result, tipResult }
     }
 }
 
+//pega cada um dos inputs e realiza o calculo e trata o erro do valor 0
 inputs.forEach( input => { 
     input.addEventListener('input', ()=>{
         if (input.name == 'value'){values.bill = input.value}; 
-        if (input.name == 'people'){values.people = input.value};                
+        if (input.name == 'people'){
+            input.style.outlineColor = "";
+            error.textContent = "";
+            values.people = input.value;
+            if (input.value == "0") {
+                error.textContent = "Can't be zero"
+                input.style.outlineColor = "red"
+            }
+        };                
         addInfos();
     })
 })
 
+//gorjetas clicadas
 tip.forEach(btn => {
     btn.addEventListener('click', ()=>{   
         values.tipValue = btn.value;
@@ -34,6 +46,7 @@ tip.forEach(btn => {
     })
 })
 
+//gorjeta customizada
 tipCustom.addEventListener('input', ()=>{
         values.tipValue = tipCustom.value;
         addInfos();
@@ -41,19 +54,20 @@ tipCustom.addEventListener('input', ()=>{
         activeBtn.classList.remove('active');
 });
 
-
+//alterna entre os backgrounds da gorjeta clicada
 function btnToggle(btn) {
     let activeBtn = btn.parentNode.querySelector('.active');
     btn.classList.add('active');
     activeBtn.classList.remove('active');
 }
 
+//adiciona os valores na tela
 function addInfos() {
     let result = values.tipResult().result;
     let tipResult = values.tipResult().tipResult;
     
-    const totalForPerson = `$${(tipResult + result).toFixed(2)}`;
-    total.textContent = totalForPerson;
+    const totalPerPerson = `$${(tipResult + result).toFixed(2)}`;
+    total.textContent = totalPerPerson;
 
     tipAmount.textContent = `$${(tipResult).toFixed(2)}`;
 
@@ -63,9 +77,17 @@ function addInfos() {
         tipAmount.textContent = "$0.00";
 
     }
-
 }
 
+
+//função reset
 reset.addEventListener('click', ()=>{
-    console.log(values)
+    inputs.forEach( input => input.value = "");
+    tip.forEach(btn => {
+        values.tipValue = 0;
+        btn.classList.remove('active');
+    });
+    tipCustom.value = "";
+    total.textContent = "$0.00";
+    tipAmount.textContent = "$0.00";
 })
